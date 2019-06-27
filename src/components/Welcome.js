c1e4e82f0c92dca8973dc6edc2fa7bce6ad1b6f7
rakeshreddy <rakesh.kps99@gmail.com>
2019-06-26 23:06:18 -0400

    first commit

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -32,7 +32,7 @@ class Welcome extends Component{
                     </a>
                 </div>
                 <div className="d-flex justify-content-center parent-div">
-                 <a href="https://drive.google.com/open?id=1M1YhnnDwnTwuEmUanJV0ijcYhBUYwb7h" target="_blank" rel="noopener noreferrer"><button className="button resume-button">Resume <i className="fa fa-file-pdf-o" aria-hidden="true"/></button></a>
+                 <a href="https://drive.google.com/open?id=1OZ6JCpW2N1QKxueU8ISoxlGj-Mg5ARa_" target="_blank" rel="noopener noreferrer"><button className="button resume-button">Resume <i className="fa fa-file-pdf-o" aria-hidden="true"/></button></a>
                  </div>
                  </h1>
                 
